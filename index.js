2679cfbe66e763fc3fb2682b98b7fda237e411dd
Lucas <lucsgdeo@gmail.com>
2025-04-29 22:28:27 -0300

    Colocando JavaScript no padrão ideal

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var data1 = new Date();
-var data2 = new Date(Date.parse("11/05/2024 23:59"));
-var dataEmDias = Math.floor((data1 - data2)/86400000);
+const data1 = new Date();
+const data2 = new Date(Date.parse("11/05/2024 23:59"));
+const dataEmDias = Math.floor((data1 - data2)/86400000);
 
-var meses = Math.floor(dataEmDias / 30);
-var dias = dataEmDias % 30;
-const resultadoFinal = meses + " meses e " + dias + " dias";
+const meses = Math.floor(dataEmDias / 30);
+const dias = dataEmDias % 30;
+const resultadoFinal = `${meses} meses e ${dias} dias`;
 
 document.getElementById("calculoTempo").innerHTML = resultadoFinal;
